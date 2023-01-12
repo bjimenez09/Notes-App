d581a8bd9a25560ccbc4f19281f35a466c84d8c3
Brandon Jimenez <bjimenezp09@gmail.com>
2023-01-11 21:33:08 -0600

    created routes.js file, added code  and added comments to server.js

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+// Listening on port 3000
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
- 
+
+// Connecting route
 require('./routes/routes')(app);
 
+// Funciton is being used to listen and bind connections
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 }); 
