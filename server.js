ff137f5afe2217679393c2dd65dd75451437a4ef
Brandon Jimenez <bjimenezp09@gmail.com>
2023-01-11 22:05:50 -0600

    added notes

--- server.js
+++ server.js
@@ -2,22 +2,23 @@ const express = require("express");
 const path = require("path");
 const api = require("./routes/index.js");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-// Middleware for parsing JSON
+// Middleware for parsing JSON and unlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
 
 app.use(express.static("public"));
 
-// GET Route for homepage
-app.get("*", (req, res) =>
+// GET route established for homepage
+app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname), "./public/index.html")
 );
 
+// GET route established for notes page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
